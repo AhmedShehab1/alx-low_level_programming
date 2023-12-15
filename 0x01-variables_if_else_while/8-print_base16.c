424c4b2b03db2f47be8e94fb9c73e805f51e1777
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-15 20:49:40 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,7 @@ int main(void)
 {
     int i = 0;
     char Array[] = {"0123456789abcdef"};
-    for ( ; i < sizeof(Array) - 1 ; i++)
+    for ( ; i < (int)sizeof(Array) - 1 ; i++)
     {
         putchar(Array[i]);
     }
