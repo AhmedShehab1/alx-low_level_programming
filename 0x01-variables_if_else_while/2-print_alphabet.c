96b0ac69af8a306cb3696981e77dff2e8dc3b5bf
AhmedShehab1 <legendbtw2003@gmail.com>
2023-12-15 08:27:45 -0800

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	for (; i < (int)sizeof(alphabet)-1; i++)
+	for (; i < (int)sizeof(alphabet) - 1; i++)
 	{
 		putchar( alphabet[i]);
 	}
