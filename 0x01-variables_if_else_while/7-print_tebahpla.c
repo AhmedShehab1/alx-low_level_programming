f4fc16bbdde22e27c2fbdcb9d76a5326f9b4ed5d
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-15 20:50:09 +0200

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -11,7 +11,7 @@ int main(void)
 {
     int i = 0;
     char Array[] = {"zyxwvutsrqponmlkjihgfedcba"};
-    for ( ; i < sizeof(Array) - 1 ; i++)
+    for ( ; i < (int)sizeof(Array) - 1 ; i++)
     {
         putchar(Array[i]);
     }
