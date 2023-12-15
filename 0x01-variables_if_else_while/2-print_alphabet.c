4cf880a542c9003da45b6895095c90ae5a869c9e
AhmedShehab1 <legendbtw2003@gmail.com>
2023-12-15 08:17:16 -0800

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	for (; i < sizeof(alphabet); i++)
+	{
+		printf("%c", alphabet[i]);
+	}
+
+	/* Print a newline character for better output readability */
+	printf("\n");
+
+	return (0);
+}
+
