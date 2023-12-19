a0d024c589ff387ccf5931f7e07083eb4abda9c4
AhmedShehab1 <legendbtw2003@gmail.com>
2023-12-19 08:06:56 -0800

    Added Second Task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 97;
+
+for ( ; i <= 122 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
