57171dba5786a1448618327cca2555e0446ca3d3
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-20 18:40:31 +0200

    .

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * 
- * 
  * add - Adds Two Integers And Returns The Result
- * 
- * 
+ * @a: First Digit
+ * @b: Second Digit
+ *
+ *Return: The Sum Of Both Digits
 */
 int add(int a, int b)
 {
