f01e7024207ccf7e6c5400f53b4ba5ed4e39ae8a
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-22 16:55:33 +0200

    .

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,12 @@
 #include "main.h"
-
 /**
- * _isupper - Checks Wether a given ascii value corresponds to an uppercase alphabet.
- * @c: ASCII Value To Be Tested
- * 
- * Return: 1 If True 0 Otherwise
- * 
+ *_isupper -Wether a given ascii value corresponds to an uppercase alphabet.
+ *@c: ASCII Value To Be Tested
+ *
+ *Return: 1 If True 0 Otherwise
+ *
 */
-int _isupper(int c)        
+int _isupper(int c)
 {
 if ((c >= 65) && (c <= 90))
 {
