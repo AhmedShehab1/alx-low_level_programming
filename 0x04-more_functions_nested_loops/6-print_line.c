cf1befac96cdd8839f80a9ca8974dd2415833871
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-22 17:15:33 +0200

    ;

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- *print_line: prints line with a length provided by user
+ *print_line- prints line with a length provided by user
  *@n: Length OF Line
- * 
+ *
 */
 void print_line(int n)
 {
 if (n > 0)
 {
-for(int i = 0; i < n; i++)
+for (int i = 0; i < n; i++)
 {
 _putchar(95);
 }
