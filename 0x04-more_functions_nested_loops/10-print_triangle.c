abbad892499914a0207015d7ab649268170cbfbb
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2023-12-22 17:48:49 +0200

    .

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- *print_triangle: prints Triangles 
+ *print_triangle- prints Triangles
  *@size: Size Of Triangle
- * 
+ *
 */
 void print_triangle(int size)
 {
-int A_intNoOfSpaces, No_Of_Stars, i;
+int A_intNoOfSpaces = size - 1, No_Of_Stars, i = 1;
 
-A_intNoOfSpaces = size - 1;
-i = 1;
 for (; i <= size; i++)
 {
-if( size > 0)
+if (size > 0)
 {
 print_spaceofTriangle(A_intNoOfSpaces--);
 for (No_Of_Stars = 0; No_Of_Stars < i; No_Of_Stars++)
@@ -28,13 +26,15 @@ _putchar('\n');
 }
 }
 /**
- *print_spaceofTriangle: prints Spaces w.r.t Diagonals
+ *print_spaceofTriangle- prints Spaces w.r.t Diagonals
  *@A_intNoOfSpaces: Spaces To Be Printed
- * 
+ *
 */
 void print_spaceofTriangle(int A_intNoOfSpaces)
 {
-for(int i = 0; i < A_intNoOfSpaces; i++)
+int i;
+
+for (i = 0; i < A_intNoOfSpaces; i++)
 {
 _putchar(' ');
 }
