b17ac6d7b3f3700a1707eda9bbce192e39ed551c
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-03-21 21:57:37 +0200

    .

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,7 +16,7 @@ size_t print_dlistint(const dlistint_t *h)
 		{
 			while (temp != NULL)
 			{
-				print("%d\n", temp->n);
+				printf("%d\n", temp->n);
 				No_Of_Elements++;
 				temp = temp->next;
 			}
