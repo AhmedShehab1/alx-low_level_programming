a440ede372ff66389dec1a2e4fb09ecb2634e829
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-04-17 22:13:05 +0200

    .

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 	return (Failure);
 
-	index = key_index(key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	head =  ht->array[index];
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
