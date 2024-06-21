ff366083e69011e9026189adde912c485aca68e1
AhmedShehab1 <legendbtw2003@gmail.com>
2024-06-21 09:36:57 +0000

    .

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Island Perimeter
+"""
+
+def island_perimeter(grid):
+	"""
+
+	Args:
+	    grid (list of list of integers): 0 represents a water zone
+					     1 represents a land zone
+					     One cell is a asquare with side length 1
+					     Grid cells are connected horizontally/vertically
+					     (not diagonally)
+					     Grid is rectangular, width and height
+				             dont exceed 100
+ 	"""
+	pass
